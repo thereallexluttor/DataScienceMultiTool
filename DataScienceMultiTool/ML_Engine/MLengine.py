73ec530dba26e4c55c186efec763748ddd317e31
Hedinyer Perucho <alucardperucho@gmail.com>
2024-02-27 15:50:57 +0100

    comments

---
+++ DataScienceMultiTool/ML_Engine/MLengine.py
@@ -0,0 +1,4 @@
+#Ml class to handle differents models and differents perspectives 
+#and approaches to work with a lot of metrics and funny models.
+
+#here it´s possible to implement libs like pytorch, mlflow and pycaret.
